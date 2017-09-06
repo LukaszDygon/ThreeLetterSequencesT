ee372624fbc91973f5709f9535fc25cff3e7dfd8
LukaszDygon <ld13281@my.bristol.ac.uk>
2017-09-06 15:51:58 +0100

    * Completed part 3. Now TLS counts for the file are stored in a dictionary * Implemented functions to print top n results and TLSs with a specific count * Refactored the code to look more readable

--- ThreeLetterSequencesT/ThreeLetterSequencesT/Program.cs
+++ ThreeLetterSequencesT/ThreeLetterSequencesT/Program.cs
@@ -7,38 +7,97 @@ using System.Threading.Tasks;
 
 namespace ThreeLetterSequencesT
 {
+    class TLSHelper
+    {
+        Dictionary<String, int> tlsCountDictionary;
+
+        public TLSHelper() {
+            this.tlsCountDictionary = new Dictionary<String, int>();
+        }
+
+        public void ConstructTLSCountDictionary(string inputFile)
+        {
+            string regexTLS = @"(\w(?=(\w\w)))";
+            string text = System.IO.File.ReadAllText(inputFile);
+            var matches = Regex.Matches(text, regexTLS, RegexOptions.IgnoreCase);
+
+            foreach (Match m in matches)
+            {
+                string matchedTLS = string.Format("{0}{1}", m.Groups[1].ToString(), m.Groups[2].ToString()).ToLower();
+                //Console.WriteLine(tls);
+                if (this.tlsCountDictionary.ContainsKey(matchedTLS))
+                {
+                    this.tlsCountDictionary[matchedTLS]++;
+                }
+                else
+                {
+                    this.tlsCountDictionary[matchedTLS] = 1;
+                }
+            }
+        }
+
+        public void PrintTLSCount(string tls)
+        {
+            tls = tls.ToLower();
+            try {
+                Console.WriteLine("Count of {0}: {1}", tls, this.tlsCountDictionary[tls]);
+            } catch (KeyNotFoundException e) {
+                Console.WriteLine("No TLS matches found for {0}", tls);
+            }
+        }
+
+        public void PrintTLSsWithCount(int numberOfOccurances)
+        {
+            var listOfHits = GetElementsWithCounts(numberOfOccurances);
+
+            Console.WriteLine("TLSs with {0} occurances:", numberOfOccurances);
+            foreach (string hit in listOfHits)
+            {
+                Console.WriteLine(hit);
+            }
+        }
+
+        public void PrintTopResults(int numberOfResults)
+        {
+            var topResults = GetTopResults(numberOfResults);
+
+            Console.WriteLine("Top {0} highest occuring results:", numberOfResults);
+            foreach (var item in topResults.Take(numberOfResults))
+            {
+                Console.WriteLine("{0} {1}", item.Key, item.Value);
+            }
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> GetTopResults(int numberOfResults)
+        {
+            var tlsCountDictionaryCopy = new Dictionary<String, int>(this.tlsCountDictionary);
+            return tlsCountDictionaryCopy.OrderByDescending(x => x.Value).Take(numberOfResults);
+        }
+
+        public List<string> GetElementsWithCounts(int count)
+        {
+            var matches = this.tlsCountDictionary.Where(kvp => kvp.Value == count).Select(kvp => kvp.Key);
+            return matches.ToList();
+        }
+
+    }
+
     class Program
     {
-        public void PrintResults(int counterTLS, int counterString)
-        {
-            Console.WriteLine("{0} TLS found", counterTLS.ToString());
-            Console.WriteLine("{0} String matches found", counterString.ToString());
-        }
-
-        public void TLSFinder(String inputFile, String regexString)
-        {
-            
-            string regexTLS = @"\w\w\w";
-            int counterTLS, counterString;
-
-            String text = System.IO.File.ReadAllText(inputFile);
-
-            counterTLS = Regex.Matches(text, regexTLS, RegexOptions.IgnoreCase).Count;
-            counterString = Regex.Matches(text, regexString, RegexOptions.IgnoreCase).Count;
-
-            PrintResults(counterTLS, counterString);
-
-            Console.ReadKey();
-        }
-
-        
-
         static void Main(string[] args)
         {
-            String inputFile = @"C:\Users\LUD\Documents\Training\ThreeLetterSequencesT\ThreeLetterSequencesT\text\SampleText.txt";
-            string regexString = "tra";
-            Program test = new Program();
-            test.TLSFinder(inputFile, regexString);
+            string inputFile = @"C:\Users\LUD\Documents\Training\ThreeLetterSequencesT\ThreeLetterSequencesT\text\SampleText.txt";
+            string testString = "the";
+            int numberOfOccurances = Int32.Parse(args[0]);
+
+            var tlsHelper = new TLSHelper();
+            tlsHelper.ConstructTLSCountDictionary(inputFile);
+
+            tlsHelper.PrintTLSCount(testString);
+            tlsHelper.PrintTopResults(10);
+            tlsHelper.PrintTLSsWithCount(numberOfOccurances);
+
+            Console.ReadKey();
         }
     }
 }
